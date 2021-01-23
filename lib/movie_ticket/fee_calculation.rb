4b16b177182dddc21cea21c955099040332f6729
ryosuke takasawa <ten.r0619@gmail.com>
2021-01-23 21:43:44 +0900

    refa

--- lib/movie_ticket/fee_calculation.rb
+++ lib/movie_ticket/fee_calculation.rb
@@ -1,5 +1,5 @@
 # チケット1枚の料金計算機能
-module FeeCalculation
+class FeeCalculation
 
   # 通常の料金計算
   def fee_calculation(day, time, user, option)
@@ -10,7 +10,7 @@ module FeeCalculation
     user_type = user.user_type
     option_price = option.option_type.price
 
-    @fee = if cinema_day == true #カッコでくくれる？
+    @fee = if cinema_day == true
       user_type.cinema_day
     elsif day_type == MovieDayType::WEEKDAY && time_type == MovieTimeType::TWILIGHT_SHOW
       user_type.weekday_twilight_show
@@ -21,14 +21,12 @@ module FeeCalculation
     elsif day_type == MovieDayType::HOLIDAY && time_type == MovieTimeType::LATE_SHOW
       user_type.holiday_late_show
     end
-
     self.cinema_citizen_weekday_cinema_day_fee(day_type, user_type, cinema_day)
 
     @fee + option_price
   end
 
   # 通常と異なる料金計算メソッド
-
   def cinema_citizen_weekday_cinema_day_fee(day_type, user_type, cinema_day)
     if user_type.class == CinemaCitizen && cinema_day == true && day_type == MovieDayType::WEEKDAY
       @fee = user_type.weekday_cinema_day
--- lib/movie_ticket/movie_ticket.rb
+++ lib/movie_ticket/movie_ticket.rb
@@ -1,13 +1,11 @@
 require_relative "./movie_time_type/movie_time_type"
 require_relative "./movie_day_type/movie_day_type"
 require_relative "./option/option"
-require_relative "./user/user.rb"
+require_relative "./user/user"
 require_relative "fee_calculation"
 
 # 映画チケット情報
 class MovieTicket
-  include FeeCalculation
-
   attr_reader :day, :time, :title, :user, :option, :fee
 
   def initialize(day_time, title, user, option)
@@ -16,6 +14,7 @@ class MovieTicket
     @title = title
     @user = User.new(user)
     @option = Option.new(option)
-    @fee = self.fee_calculation(@day, @time, @user, @option)
+    fee_obj = FeeCalculation.new
+    @fee = fee_obj.fee_calculation(@day, @time, @user, @option)
   end
 end
